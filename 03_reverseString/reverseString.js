2fd1aafac7f295e0aa6492bdc0bc0e4aec27078b
Sheepnore <a83840066@gmail.com>
2024-08-11 18:19:22 +0800

    03 exercise completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,26 @@
-const reverseString = function() {
+// First, turn the string into a array using for loop
+// Then, loop thru the array and return the last item in the array
+// lastly, remove the last item in the array
 
+const reverseString = function(string){
+
+    let newString = [];
+    let reversedString = '';
+    
+
+    for (let i = 0; i < string.length; i++){
+      newString.push(string[i]);
+     }
+
+    let tempString = [...newString]; // deep copy using Spread operator `...`
+
+     for (let i = 0; i < newString.length; i++){
+      
+      reversedString += tempString.at(-1);
+      tempString.splice(-1,1);
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
