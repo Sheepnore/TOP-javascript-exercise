3eb228074a403e18added24025d3b0c27603db4f
Sheepnore <a83840066@gmail.com>
2024-08-12 20:13:00 +0800

    07 exercise completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+  let degree = 0;
+  degree = Number(((Fahrenheit - 32) / 1.8).toFixed(1)) 
+  return degree
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celsius){
+  let degree = 0;
+  degree = Number(((Celsius * 1.8) + 32).toFixed(1));
+  return degree;
 };
 
 // Do not edit below this line
