240d07cc6da91d8e346f27364d4b681715014d05
Sheepnore <a83840066@gmail.com>
2024-08-12 16:58:00 +0800

    06 exercise completed, it took me hours to solve it. Glad I did it

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,23 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  let isLeapYear = false;
+  // if a number is divisible by 100 or 400, it is also divisble by 4;
+  let isDivisibleBy4 = year % 4 === 0;
+  let isDivisibleBy100 = year % 100 === 0;
+  let isDivisibleBy400 = year % 400 === 0;
 
+  if(isDivisibleBy4){
+    if (isDivisibleBy100 && isDivisibleBy400){
+      isLeapYear = true;
+    }
+    else if (isDivisibleBy100){
+      isLeapYear = false;
+    }
+    else{
+      isLeapYear = true;
+    }
+  }
+  
+  return isLeapYear;
 };
 
 // Do not edit below this line
