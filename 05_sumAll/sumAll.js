8b23f412617250dab3efcad37fa467ca5baa781c
Sheepnore <a83840066@gmail.com>
2024-08-12 14:36:31 +0800

    05 exercise completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let largerNum;
+  let smallerNum;
+  let sumBoth = num1 + num2;
 
+  if (num1 > 0 && num2 > 0 && Number.isInteger(sumBoth) && !isNaN(sumBoth)){
+    if (num2 > num1){
+      largerNum = num2;
+      smallerNum = num1;
+    }
+    else {
+      largerNum = num1;
+      smallerNum = num2;
+    }
+
+    return (largerNum * (smallerNum + largerNum))/2
+  }
+  else{
+    return 'ERROR'
+  }
+  
 };
 
 // Do not edit below this line
