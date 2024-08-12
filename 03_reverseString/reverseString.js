5d81a785e8ce02cf3dca443af0b616c619a50bf1
Sheepnore <a83840066@gmail.com>
2024-08-12 13:56:52 +0800

    04 exercise completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -7,15 +7,14 @@ const reverseString = function(string){
     let newString = [];
     let reversedString = '';
     
-
+    // 
     for (let i = 0; i < string.length; i++){
       newString.push(string[i]);
      }
 
-    let tempString = [...newString]; // deep copy using Spread operator `...`
+    let tempString = [...newString]; // `...` Deep copy. `tempString` is created so that when looping thru the array in the code below, the original array will not be affected  
 
      for (let i = 0; i < newString.length; i++){
-      
       reversedString += tempString.at(-1);
       tempString.splice(-1,1);
     }
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeItems){
+  let temp = array;
+
+  for (let i = 0; i<removeItems.length; i++){
+    temp = temp.filter(item => item!== removeItems[i]);  
+  }
+
+  return temp
+
+  //
+  // let temp;
+  //
+  //  MISTAKE!! It only modifies the array by the last item in removeItems. 
+  // for (let i = 0; i<removeItems.length; i++){
+  //  temp = array.filter(item => item!== removeItems[i]);  
+  // }
+  //
+  // return temp */
+
 };
 
 // Do not edit below this line
