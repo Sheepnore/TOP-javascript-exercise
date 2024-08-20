acae152d63f09b0c4c1498537d323856faf88a93
Sheepnore <a83840066@gmail.com>
2024-08-20 21:03:03 +0800

    09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,18 @@
-const palindromes = function () {
+const palindromes = function(string){
+  const arr = string.split('')
+  let cleanArray = arr.filter((char) => (char >= 'a' && char<= 'z') || (char >= 'A' && char <='Z'));
+
+  const cleanString = cleanArray.join('').toLowerCase();
+  const reversed = cleanArray.reverse();
+
+  const cleanReversedString = reversed.join('').toLowerCase();
+  
+  if (cleanString === cleanReversedString){
+    return true
+  }
+  else{
+    return false
+  }
 
 };
 
