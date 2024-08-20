dad2cbafd5370a20b86a1b9154361cc8e15d8cab
Sheepnore <a83840066@gmail.com>
2024-08-21 00:11:18 +0800

    09 all tests passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,20 +1,22 @@
 const palindromes = function(string){
-  const arr = string.split('')
-  let cleanArray = arr.filter((char) => (char >= 'a' && char<= 'z') || (char >= 'A' && char <='Z'));
+  const arr = string.split('');
 
+  let cleanArray = arr.filter( char => { 
+    return (char >= '0' && char <= '9' )||(char >= 'a' && char<= 'z') || (char >= 'A' && char <='Z')});
+  
+  
   const cleanString = cleanArray.join('').toLowerCase();
   const reversed = cleanArray.reverse();
 
   const cleanReversedString = reversed.join('').toLowerCase();
-  
+
+  console.log(cleanString,cleanReversedString);
   if (cleanString === cleanReversedString){
     return true
   }
   else{
     return false
   }
-
 };
-
 // Do not edit below this line
 module.exports = palindromes;
