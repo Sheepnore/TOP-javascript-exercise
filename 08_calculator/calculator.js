943086be067262b4287c7174e6f2982bfafa1f4c
Sheepnore <a83840066@gmail.com>
2024-08-20 13:43:43 +0800

    08 exercise completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,54 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a -b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let temp = 0;
+  if (nums.length === 0){
+    return 0
+  }
+  else{
+    nums.forEach((num)=>{
+      temp += num;
+    })
+    return temp;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let result = 0;
+  if (nums.length === 0){
+    return 0
+  }
+  else{
+    result = nums.reduce((accumulator,currentValue)=>{
+      return accumulator * currentValue;
+    },)
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let arr = [];
+  if (a === 0){
+    return 1
+  }
+
+  for (let i = a; i>=1; i--){
+    arr.push(i)
+  }
+  let factor = arr.reduce((accumulator, currentValue)=> {
+    return accumulator * currentValue;
+  },)
+  return factor;
 };
 
 // Do not edit below this line
