6fc72d246badbecadf46f56ebf5c12dcb9041a8f
Sheepnore <a83840066@gmail.com>
2024-08-21 22:00:32 +0800

    11 exe completed (might not look elegant)

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(arrObj){
 
+  let result = arrObj.map(obj => {
+    return  obj.title
+  })
+  return result;
 };
 
 // Do not edit below this line
