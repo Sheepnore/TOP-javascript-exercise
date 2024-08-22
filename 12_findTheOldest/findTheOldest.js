bebae52ccea71dc356639b1a053e281a4a734ac3
Sheepnore <a83840066@gmail.com>
2024-08-22 13:23:14 +0800

    12 ex completed (all execises done! )

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,33 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrObj) {
+  // Add `years` prop to object
+  const modified = arrObj.map( obj => {
+    if (!obj.yearOfDeath){
+      const currentDate = new Date();
+      const currentFullYear = currentDate.getFullYear();
+      obj.yearOfDeath = currentFullYear;
+    }
 
+    obj.years = obj.yearOfDeath - obj.yearOfBirth;
+    return obj
+  })
+
+  // Find `oldest` value
+  let oldest = 0;
+  modified.forEach(obj => {
+    if (obj.years > oldest){
+      oldest = obj.years;
+    }
+  })
+
+  // Find the obj that matches `oldest` value 
+  let returnResult;
+  modified.forEach( obj => {
+    if (obj.years === oldest){
+      returnResult = obj
+    }
+  })
+
+  return returnResult;
 };
 
 // Do not edit below this line
